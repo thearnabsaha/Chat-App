0400fefb1c8c4884f6cc52d7ebd19e444ba15af1
thearnabsaha <thearnabsaha201@gmail.com>
2025-05-28 11:06:20 +0530

    feat: add backend-common and db packages with initial setup and user model

--- apps/express-server/src/index.ts
+++ apps/express-server/src/index.ts
@@ -2,15 +2,17 @@ import express from 'express';
 const app = express();
 import dotenv from 'dotenv';
 dotenv.config();
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3001;
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
+import {SigninSchema,SignupSchema} from '@workspace/common/types'
 const morganFormat = ':method :url :status :response-time ms';
-
+import {JWT_SECRET} from '@workspace/backend-common/config'
 app.use(morgan(morganFormat));
 app.use(helmet());
+import { prisma } from '@workspace/db/client'
 
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
@@ -38,5 +40,25 @@ app.get('/health', async (req, res) => {
     responseTime: `${Date.now() - start}ms`,
   };
   res.status(200).json(healthcheck);
+});
+
+app.post('/signup', async (req, res) => {
+  const result = SignupSchema.safeParse(req.body);
+  if (!result.success) {
+    res.send(result.error.format());
+  } else {
+    const user=await prisma.user.create({
+      data:req.body
+    });
+    res.send(user);
+  }
+});
+app.post('/signin', (req, res) => {
+  const result = SigninSchema.safeParse(req.body);
+  if (!result.success) {
+    res.send(result.error.format());
+  } else {
+    res.send(result);
+  }
 });
 app.listen(port, () => console.log('> Server is up and running on port: ' + port));
