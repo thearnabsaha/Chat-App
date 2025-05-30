6243388fa112faa86665eb7cf973ed446f3789a9
thearnabsaha <thearnabsaha201@gmail.com>
2025-05-30 22:54:33 +0530

    refactor: remove commented-out CORS options code

--- apps/express-server/src/index.ts
+++ apps/express-server/src/index.ts
@@ -21,11 +21,6 @@ app.use(cors({
   credentials: true,
 }));
 
-// app.options('*', cors({
-//   origin: process.env.CORS_ORIGIN,
-//   credentials: true,
-// }));
-
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
