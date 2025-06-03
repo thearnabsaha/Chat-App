ac8e7597d9c1eab119a87143eddeea2818b794c0
thearnabsaha <thearnabsaha201@gmail.com>
2025-06-03 13:52:53 +0530

    feat: add FindRooms, FindRoom, UpdateRoom, and deleteRooms controllers with user validation, update room routes

--- apps/express-server/src/controllers/room.controller.ts
+++ apps/express-server/src/controllers/room.controller.ts
@@ -23,3 +23,113 @@ export const CreateRoom = async (req: Request, res: Response) => {
         res.status(500).json({error})
     }
 }
+export const FindRooms = async (req: Request, res: Response) => {
+    try {
+        const user=await prisma.user.findFirst({where:{username:req.body.username}})
+        if(!user){
+            res.status(400).json({message:"User is not Valid!"})
+            return;
+        }
+        const room=await prisma.room.findFirst({where:{slug:req.body.slug}})
+        if(room){
+            res.status(411).json({message:"This Room Already Exists!"})
+            return;
+        }
+        const room1 = await prisma.room.create({data:{
+            slug:req.body.slug,
+            adminId:user?.id
+        }})
+        res.status(201).json({message:room1.slug+" is created!"})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error})
+    }
+}
+export const FindRoom = async (req: Request, res: Response) => {
+    try {
+        const user=await prisma.user.findFirst({where:{username:req.body.username}})
+        if(!user){
+            res.status(400).json({message:"User is not Valid!"})
+            return;
+        }
+        const room=await prisma.room.findFirst({where:{slug:req.body.slug}})
+        if(room){
+            res.status(411).json({message:"This Room Already Exists!"})
+            return;
+        }
+        const room1 = await prisma.room.create({data:{
+            slug:req.body.slug,
+            adminId:user?.id
+        }})
+        res.status(201).json({message:room1.slug+" is created!"})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error})
+    }
+}
+export const UpdateRoom = async (req: Request, res: Response) => {
+    try {
+        const user=await prisma.user.findFirst({where:{username:req.body.username}})
+        if(!user){
+            res.status(400).json({message:"User is not Valid!"})
+            return;
+        }
+        const room=await prisma.room.findFirst({where:{slug:req.body.slug}})
+        if(room){
+            res.status(411).json({message:"This Room Already Exists!"})
+            return;
+        }
+        const room1 = await prisma.room.create({data:{
+            slug:req.body.slug,
+            adminId:user?.id
+        }})
+        res.status(201).json({message:room1.slug+" is created!"})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error})
+    }
+}
+export const deleteRoom = async (req: Request, res: Response) => {
+    try {
+        const user=await prisma.user.findFirst({where:{username:req.body.username}})
+        if(!user){
+            res.status(400).json({message:"User is not Valid!"})
+            return;
+        }
+        const room=await prisma.room.findFirst({where:{slug:req.body.slug}})
+        if(room){
+            res.status(411).json({message:"This Room Already Exists!"})
+            return;
+        }
+        const room1 = await prisma.room.create({data:{
+            slug:req.body.slug,
+            adminId:user?.id
+        }})
+        res.status(201).json({message:room1.slug+" is created!"})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error})
+    }
+}
+export const deleteRooms = async (req: Request, res: Response) => {
+    try {
+        const user=await prisma.user.findFirst({where:{username:req.body.username}})
+        if(!user){
+            res.status(400).json({message:"User is not Valid!"})
+            return;
+        }
+        const room=await prisma.room.findFirst({where:{slug:req.body.slug}})
+        if(room){
+            res.status(411).json({message:"This Room Already Exists!"})
+            return;
+        }
+        const room1 = await prisma.room.create({data:{
+            slug:req.body.slug,
+            adminId:user?.id
+        }})
+        res.status(201).json({message:room1.slug+" is created!"})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error})
+    }
+}
--- apps/express-server/src/routes/room.routes.ts
+++ apps/express-server/src/routes/room.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { jwtAuth } from "../middlewares/jwtAuth";
-import { CreateRoom } from "../controllers/room.controller";
+import { CreateRoom, deleteRoom, deleteRooms, FindRoom, FindRooms, UpdateRoom } from "../controllers/room.controller";
 
 const router = Router();
 
 router.post("/create", jwtAuth, CreateRoom);
+router.post("/find", jwtAuth, FindRooms);
+router.post("/find/:id", jwtAuth, FindRoom);
+router.post("/update/:id", jwtAuth, UpdateRoom);
+router.post("/delete/:id", jwtAuth, deleteRoom);
+router.post("/delete", jwtAuth, deleteRooms);
 
 export default router;
