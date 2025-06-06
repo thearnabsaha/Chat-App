d1c03a96a7bdebc6ace38decc10a3d223fc1c1e1
thearnabsaha <thearnabsaha201@gmail.com>
2025-06-06 17:04:28 +0530

    feat: enhance Room component layout and messaging interface

--- apps/web/app/room/[slug]/page.tsx
+++ apps/web/app/room/[slug]/page.tsx
@@ -1,11 +1,36 @@
 "use client"
 import { useRoomStore } from "@/lib/store/roomStore"
+import { Button } from "@workspace/ui/components/button"
+import { Input } from "@workspace/ui/components/input"
 
 const Room = () => {
     const { room } = useRoomStore()
-
+// {room?.RoomId}
     return (
-        <div>{room?.RoomId}</div>
+        <div className="flex justify-center mt-10">
+            <div className="w-[60vw] h-[70vh] border border-ring rounded-md">
+                <div>
+                    <h1>Real Time Chat</h1>
+                    <p>This Room is Created by Arnab (@thearnabsaha)</p>
+                </div>
+                <div>
+                    <h1>ROOM CODE: GH6GI7</h1>
+                    <p>Users: 10/12</p>
+                </div>
+                <div>
+                    <p>My msgs</p>
+                    <p>Your msgs</p>
+                    <p>Your msgs</p>
+                    <p>My msgs</p>
+                    <p>My msgs</p>
+                    <p>Your msgs</p>
+                </div>
+                <div>
+                    <Input placeholder="Type a Msssage here..."/>
+                    <Button>Send</Button>
+                </div>
+            </div>
+        </div>
     )
 }
 
