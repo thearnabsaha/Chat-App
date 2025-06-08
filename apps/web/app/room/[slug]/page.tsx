6e2fd937539657a4e6c547cd56c46e649f0eb18d
thearnabsaha <thearnabsaha201@gmail.com>
2025-06-08 13:13:53 +0530

    feat: fetch and display messages in real-time chat room

--- apps/web/app/room/[slug]/page.tsx
+++ apps/web/app/room/[slug]/page.tsx
@@ -16,6 +16,9 @@ import {
 } from "@workspace/ui/components/form"
 import { useUserStore } from "@/lib/store/userStore"
 import { Skeleton } from "@workspace/ui/components/skeleton"
+import axios from "axios"
+import { BACKEND_URL } from "@/lib/config"
+import toast from "react-hot-toast"
 const inputSchema = z.object({
     msg: z.string().min(1),
 })
@@ -36,8 +39,17 @@ const Room = () => {
         InputForm.reset()
     }
     const params=useParams()
+    type Message = {
+        id: string;
+        userId: string;
+        roomId: string;
+        message: string;
+    };
+    const [message, setMessage] = useState<Message[]>([])
+    const [userId, setUserId] = useState("")
     useEffect(() => {
         const roomid = localStorage.getItem("roomId")
+        const token = localStorage.getItem("token")
         setroomId(roomid)
         if (!roomid) {
             router.push(`/dashboard`)
@@ -45,29 +57,49 @@ const Room = () => {
         if (roomid!=params.slug) {
             router.push(`/dashboard`)
         }
+        axios.get(`${BACKEND_URL}/chat/${roomid}`, { headers: { Authorization: token } })
+            .then((e) => {
+                // toast.success("Room Updated")
+                // console.log(e.data.allchats)
+                setMessage(e.data.allchats)
+                // window.location.reload()
+            }).catch((e) => {
+                console.log(e)
+            })
+        axios.get(`${BACKEND_URL}/me`, { headers: { Authorization: token } })
+            .then((e) => {
+                // toast.success("Room Updated")
+                // console.log(e.data.message.id)
+                setUserId(e.data.message.id)
+                // setMessage(e.data.allchats)
+                // window.location.reload()
+            }).catch((e) => {
+                console.log(e)
+            })
     }, [])
     return (
         <div className="flex justify-center mt-10 font-mono">
             <div className="w-[60vw] h-[72vh] border border-ring rounded-md p-10">
                 <div>
                     {user?<h1 className="text-3xl font-bold pb-1">Real Time Chat</h1>:<Skeleton className=" w-96 h-10 mb-1 rounded-md"/>}
-                    {user?<p className="font-bold text-ring pb-5">This Room is Created by {user.name} (@{user.username})</p>:<Skeleton className=" w-[30vw] h-5 rounded-md mb-5"/>}
+                    {user?<p className="font-bold text-ring pb-5">This Room is Created by <span className=" capitalize">{user.name}</span> (@{user.username})</p>:<Skeleton className=" w-[30vw] h-5 rounded-md mb-5"/>}
                 </div>
                 <div className="bg-accent flex justify-between px-10 py-5 rounded-md text-ring">
                     {roomId?<h1>ROOM CODE: {roomId}</h1>:<Skeleton className="w-40 h-5 bg-ring"/>}
                     <p>Users: 10/12</p>
                 </div>
                 <div className="border border-ring my-5 h-[40vh] rounded-md flex flex-col items-end overflow-auto py-5">
-                    <p className=" py-2 px-5 rounded-lg mx-5 my-1 bg-primary text-secondary max-w-96">My msgs</p>
-                    <p className=" py-2 px-5 rounded-lg mx-5 my-1 bg-accent self-start max-w-96">Your msgs</p>
-                    <p className=" py-2 px-5 rounded-lg mx-5 my-1 bg-accent self-start max-w-96">Your msgs</p>
-                    <p className=" py-2 px-5 rounded-lg mx-5 my-1 bg-primary text-secondary max-w-96">My msgs</p>
-                    <p className=" py-2 px-5 rounded-lg mx-5 my-1 bg-accent self-start max-w-96">Your msgs</p>
-                    <p className=" py-2 px-5 rounded-lg mx-5 my-1 bg-primary text-secondary max-w-96">My msgs Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi odio modi harum nam deleniti blanditiis assumenda doloribus perspiciatis reprehenderit numquam.</p>
-                    <p className=" py-2 px-5 rounded-lg mx-5 my-1 bg-accent self-start max-w-96">Your msgs</p>
-                    <p className=" py-2 px-5 rounded-lg mx-5 my-1 bg-accent self-start max-w-96">Your msgs Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat excepturi repellat eligendi quia molestias temporibus accusantium, non autem veniam praesentium.</p>
-                    <p className=" py-2 px-5 rounded-lg mx-5 my-1 bg-primary text-secondary max-w-96">My msgs</p>
-                    <p className=" py-2 px-5 rounded-lg mx-5 my-1 bg-primary text-secondary max-w-96">My msgs</p>
+                    {
+                        message.map((e)=>{
+                            return(
+                                <div key={e.id+e.userId+e.roomId+e.message} className="w-full flex flex-col items-end">
+                                    {e.userId===userId?
+                                    <p className=" py-2 px-5 rounded-lg mx-5 my-1 bg-primary text-secondary max-w-96 capitalize">{e.message}</p>
+                                    :<p className="py-2 px-5 rounded-lg mx-5 my-1 bg-accent self-start max-w-96">{e.message}</p>}
+                                </div>
+                            )
+                        })
+                    }
                 </div>
                 <div className="flex">
                     <Form {...InputForm}>
