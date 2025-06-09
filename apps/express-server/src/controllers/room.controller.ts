8b405ac3eab20d1ea9bab40b352d714807057643
thearnabsaha <thearnabsaha201@gmail.com>
2025-06-09 22:36:15 +0530

    fix: rename variable for clarity in CreateRoom function

--- apps/express-server/src/controllers/room.controller.ts
+++ apps/express-server/src/controllers/room.controller.ts
@@ -13,13 +13,13 @@ export const CreateRoom = async (req: Request, res: Response) => {
             res.status(411).json({ message: "This Room Already Exists!" })
             return;
         }
-        const room1 = await prisma.room.create({
+        const newRoom = await prisma.room.create({
             data: {
                 slug: req.body.slug,
                 adminId: user?.id
             }
         })
-        res.status(201).json({ message: room1.slug + " is created!" })
+        res.status(201).json({ message: newRoom.slug + " is created!" })
     } catch (error) {
         console.log(error)
         res.status(500).json({ error })
